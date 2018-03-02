e69c4f1395b6926068aeba682400d512f2d9ecbe
AraceliCumen <aycuevam@gmail.com>
2018-03-02 18:33:17 -0500

    Subiendo estructura de carpetas

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var server = app.listen(3000, listening);
+
+function listening() {
+  console.log('Server on');
+}
+
+
+//Desde la carpeta public vamos a servir nuestra pagina web
+app.use(express.static('public'));
