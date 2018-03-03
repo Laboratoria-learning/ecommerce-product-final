c0ddde703cfd0730a95bcab78ef9237c559d1183
betsyvies <ing.betsyvies@gmail.com>
2018-03-03 11:34:07 -0500

    añadiendo acceso a la data

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+/* Array de accesorios de celulares */
+
+
+
+/* Categoría accesorios para celulares */
+let xhr = $.get('https://api.mercadolibre.com/sites/MPE/search?q=carcasses&condition=new');
+xhr.done(function(data) {
+  console.log(data);
+});
