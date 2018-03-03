841c9b8c27f064aabb873da813290c155d18fe7c
betsyvies <ing.betsyvies@gmail.com>
2018-03-03 18:01:37 -0500

    añadiendo body-parser

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+// Body parser 
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Archivos estaticos  (js y css)
 app.use(express.static(__dirname + '/public'));
@@ -9,4 +16,4 @@ app.get('/', function(req, res) {
   res.sendfile('index.html');
 });
 
-app.listen(3000);
+app.listen(port);
