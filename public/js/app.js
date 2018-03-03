df34e3a4169c3bc427469d3c3ff26c9175c353cf
carlacentenor <carlacentenor@gmail.com>
2018-03-03 09:07:07 -0500

    spa funcional

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,77 @@
-$(document).ready(function() {
+$(document).ready(function() {// 2DCF9C
+  $('.carousel').carousel();
+
+  page('/Disney Infinity', products);
+  page('/Flippers y Arcade', products);
+  page('/Nintendo', products);
+  page('/PlayStation', products);
+  page('/SEGA', products);
+  page('/Tarjetas Prepagas para Juegos', products);
+  page('/Videojuegos', products);
+  page('/Xbox', products);
+  page('/Otras Marcas', products);
+  page('/public/index.html', home);
+  page();
+
   $.ajax({
-    url: 'https://api.mercadolibre.com/categories/MPE1430', // cambiar el
+    url: 'https://api.mercadolibre.com/categories/MLA1144', // cambiar el
     success: function(data) {
       let categories = data.children_categories;
       console.log(categories);
-      categories.forEach(element => {
-        let template = `<div data-id="${element.id}"  class="card" style="width: 18rem;">
-       <img class="card-img-top" src="" alt="Card image cap">
-       <div class="card-body">
-         <p class="card-text">${element.name}</p>
-       </div>
-     </div>`;
+      categories.forEach((element, index) => {
+        let template = `<li class="nav-item" data-id="${element.id}" >
+       <a class="nav-link" href="/${element.name}">${element.name}</a>
+        </li>`;
         $('#container').append(template);
       });
     }
   });
+ 
+  function products(ctx) {
+    $('.row').html('');
+    let categories = ctx.path;
+    let name = categories.substr(1);
+    console.log(name);
 
+    $.ajax({
+      url: `https://api.mercadolibre.com/sites/MPE/search?condition=new&q=${name}`, // cambiar el
+      success: function(data) {
+        let categories = data.results;
+        categories.forEach((element)=>{
+          template(element);   
+        });
+      }  
+    });
+  }
+  function home() {
+    $('.row').html('');
+    $.ajax({
+      url: 'https://api.mercadolibre.com/sites/MPE/search?category=MPE1144', // cambiar el
+      success: function(data) {
+        let productResults = data.results;
+        productResults.forEach((element, index) => {
+          template(element);
+        });
+      }
+    }); 
+  }
+ 
 
-  /*  $.ajax({
-    url: 'https://api.mercadolibre.com/sites/MLA/search?category=MLA5726', // cambiar el
-    success: function(data){
-      console.log(data.results)
-    }
-  }); */
+  function template(element) {
+    let template = `<div class="col-lg-3">
+        <div class="card h3">
+        <div class="text-center"><img class="img-r" src="${element.thumbnail}" alt="Card image cap"></div>
+        
+        <div class="card-body text-center">
+          <h5 class="card-title">${element.title}</h5>
+          <p class="card-text">Precio : ${element.price}</p>
+          <a href="#" class="btn btn-primary">Buy</a>
+        </div>
+      
+        </div>
+      </div>`;
+    $('.row').append(template);
+  }
+
+  home();
 });
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3005;
 app.listen(port, () => {
   console.log('Server running on port ' + port + '!');
 });
