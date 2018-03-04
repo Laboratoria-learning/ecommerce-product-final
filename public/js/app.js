c450283db174a86d8ce41e2349e1eb046a254a80
betsyvies <ing.betsyvies@gmail.com>
2018-03-03 21:04:33 -0500

    añadiendo articulos

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,37 @@
-/* Categoría accesorios para celulares */
-let xhr = $.get('https://api.mercadolibre.com/sites/MPE/search?q=carcasses&condition=new');
-xhr.done(function(data) {
-  console.log(data);
-});
+function begin() {
+  let $products = $('#products');
+  
+  /* Categoría antiguedades */
+  let xhr = $.get('https://api.mercadolibre.com/sites/MPE/search?q=Antigüedades&condition=new');
+
+  xhr.done(function(data) { 
+    console.log(data);
+    $products.html('');
+
+    let dataArticles = data.results;
+    dataArticles.forEach(element => {
+      let id = element.id;
+      let img = element.thumbnail;
+      let title = element.title;
+      let price = element.price;
+      let availableQuantity = element.available_quantity;
+      let soldQuantity = element.sold_quantity; 
+
+      $products.append(`
+      <div class="col-4" data-id="${id}">
+      <div class="col-12">
+          <img src="${img}" alt="title" class="bor-rad">
+      </div>
+      <div class="col-12">
+          <p>${title}</p>
+          <p>${price}</p>
+          <p>${availableQuantity}</p>
+          <p>${soldQuantity}</p>
+      </div>
+      </div>
+      `);
+    });
+  });
+}
+
+$(document).ready(begin);
