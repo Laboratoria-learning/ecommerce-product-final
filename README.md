d7d9c49eac3f32f5e4872f320cd27d47391a9255
betsyvies <ing.betsyvies@gmail.com>
2018-03-05 10:43:27 -0500

    añadiendo readme

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # E-commerce
-***
-
 
 ### Objetivo:
 
@@ -13,14 +11,16 @@
 
 ### Demo:
 
-![Sin titulo](public/assets/docs/index.PNG)
-
+![Sin titulo](public/assets/docs/index.png)
 
 * Cuando seleccionas un de los item de la barra de navegación por ejemplo alcatel te mostrará lo siguiente: 
 
-![Sin titulo](public/assets/docs/alcatel.PNG)
+![Sin titulo](public/assets/docs/alcatel.png)
 
 En la anterior imagen se puede observar que se cargaron todos los celulares del modelo alcatel, este proceso se realiza para los demás modelos de celulares.
+
+
+![Sin titulo](public/assets/docs/product.png)
 
 ### Desarollado para
 
--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 const express = require('express');
-// Body parser 
-// const bodyParser = require('body-parser');
 
 const app = express();
-// const port = process.env.PORT || 3000;
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: false}));
 
 var port = process.env.PORT || 8080;
 
-// var todoRoutes = require('./routes/ruta');
-
-app.use(express.static(__dirname + '/views'));
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendfile('index.html');
 });
-// app.get('/alcatel', todoRoutes);
-
 
 app.listen(port);
