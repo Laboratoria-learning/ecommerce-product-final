712268901cea1e3c2db8959a2d0a4eac722ab315
betsyvies <ing.betsyvies@gmail.com>
2018-03-05 11:05:02 -0500

    añadiendo dirname

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-
 const app = express();
 
 var port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
   res.sendfile('index.html');
